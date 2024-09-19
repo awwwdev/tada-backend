a74ff29acaf82a279bd637be0e62d0d57d3b7e67
awwwdev <->
2024-09-19 12:52:20 +0330

    fix: error messages

--- auth/ensureLoggedIn.ts
+++ auth/ensureLoggedIn.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
+import ERRORS from '../errors';
 
 export default function ensureLoggedIn(req: Request, res: Response, next: any) {
   if (req.isAuthenticated()) return next();
-  res.status(401).json({ message: 'Not Authenticated' });
+  res.status(401).json(ERRORS.NOT_AUTHENTICATED);
 }
