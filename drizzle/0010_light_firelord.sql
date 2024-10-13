0ccda33b8141ac3f63adddeedced7ffb1f950f1f
awwwdev <->
2024-10-13 15:12:51 +0330

    fix: migrate db

---
+++ drizzle/0010_light_firelord.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "folder" RENAME COLUMN "emojies" TO "emojis";--> statement-breakpoint
+ALTER TABLE "list" RENAME COLUMN "emojies" TO "emojis";--> statement-breakpoint
+ALTER TABLE "task" RENAME COLUMN "emojies" TO "emojis";
---
+++ drizzle/0011_cold_changeling.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "list" RENAME COLUMN "order_in_panel" TO "order_in_folder";
